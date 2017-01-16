d1efc53cebaedc87b3c8a664338aab4b637878d7
adarshasun <adarshasun@gmail.com>
2017-01-16 10:33:36 +0545

    Add files via upload

---
+++ adarsha2.c
@@ -0,0 +1,25 @@
+/*
+Student name:Adarsha Sunchuri
+program:program to display following patterns
+roll no:03
+Date:16 Jan,2017
+*/
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	
+	printf("Enter the number of rows:");
+	
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
