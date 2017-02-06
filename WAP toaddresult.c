0deb2a4719995356c44fab8faee9c4d26805278a
adarshasun <adarshasun@gmail.com>
2017-02-06 10:38:44 +0545

    Add files via upload

---
+++ WAP toaddresult.c
@@ -0,0 +1,23 @@
+/*
+Name:Adarsha Sunchuri
+Lab Sheet:
+Program:WAP to create a structure with twointeger number call the structure in main method and add and display the result
+Date:16 Jan,2017
+*/
+#include<stdio.h>
+struct sum
+{
+int a;
+int b;
+}result;
+int main()
+{
+int add;
+printf("enter the first number\n");
+scanf("%d",&result.a);
+printf("enter the second number\n");
+scanf("%d",&result.b);
+add=result.a+result.b;
+printf("the sum is :%d\n",add);
+return 0;
+}
